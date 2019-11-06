59bcb55b0b76600ca6843e52916ea6a0660504e8
alexdzius <alex.dzius@digipen.edu>
2019-11-06 14:58:04 -0800

    second scenef

---
+++ pongdotfun/Assets/Scripts/changelevelafterwin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changelevelafterwin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void NextScene()
+    {
+        SceneManager.LoadScene("game");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
