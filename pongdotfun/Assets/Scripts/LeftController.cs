c06e295e3a640e0d570458437267b8a0b22155f7
CJ <charles.sweatt>
2019-11-07 12:25:02 -0800

    Added scripts

---
+++ pongdotfun/Assets/Scripts/LeftController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    public float Speed;
+    Vector2 movement = new Vector2();
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        movement.y = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetAxis("VerticalLeft") > 0.1)
+        {
+
+            myRigidBody.AddForce(movement);
+        }
+
+        if (Input.GetAxis("VerticalLeft") < -0.1)
+        {
+            myRigidBody.AddForce(-movement);
+        }
+
+    }
+}
