ac0758cede29069cdf99e339389777adb9e2ef69
alexdzius <alex.dzius@digipen.edu>
2019-11-07 14:28:41 -0800

    uwu

---
+++ pongdotfun/Assets/Scripts/ScoreSystem2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ScoreSystem2 : MonoBehaviour
+{
+    public int scoreValue2;
+    public Text ScoreText;
+    public GameObject Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreValue2 = 0;
+        ScoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ball.transform.position.x <= 11.38f && Ball)
+        {
+            scoreValue2 += 1;
+            ScoreText.text = "Score: " + scoreValue2;
+            Ball.transform.position = new Vector2(0, 0);
+        }
+        if (scoreValue2 > 11)
+        {
+            SceneManager.LoadScene("winscreen");
+        }
+    }
+}
