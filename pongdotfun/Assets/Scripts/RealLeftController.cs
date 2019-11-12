84b8ce10232e97a79f2b895ca9191a7935752b98
alexdzius <alex.dzius@digipen.edu>
2019-11-12 13:43:18 -0800

    u

---
+++ pongdotfun/Assets/Scripts/RealLeftController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealLeftController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    public float Speed;
+    Vector2 movementy = new Vector2(0, 10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetAxis("VerticalLeft") > 0.1 && transform.position.y < 3.8)
+        {
+       
+            transform.position += new Vector3(0,5,0) * Time.deltaTime;
+        }
+        else if (Input.GetAxis("VerticalLeft") < -0.1 && transform.position.y > -3.8)
+        {
+            transform.position += new Vector3(0, -5, 0) * Time.deltaTime;
+        } 
+        
+
+    }
+}
---
+++ pongdotfun/Assets/Scripts/RealRightController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealRightController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    public float Speed;
+    Vector2 movementy = new Vector2(0, 10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetAxis("VerticalRight") > 0.1)
+        {
+
+            transform.position += new Vector3(0, 5, 0) * Time.deltaTime;
+        }
+        if (Input.GetAxis("VerticalRight") < -0.1)
+        {
+            transform.position += new Vector3(0, -5, 0) * Time.deltaTime;
+        }
+    }
+}
